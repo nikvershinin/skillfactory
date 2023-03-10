0d187d88e49e5f3ef36c11517a2db12c799b8a47
nikvershinin <nik_vershinin@mail.ru>
2023-03-10 17:25:15 +0500

    add new pages

--- NewsPortal/NewsPaper/aboutme/urls.py
+++ NewsPortal/NewsPaper/aboutme/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import *
 
 urlpatterns = [
-    path('index/', index),
+    path('index/', index, name='index'),
+    path('contacts/', contacts, name='contacts'),
+    path('aboutme/', aboutme, name='aboutme'),
 ]
--- NewsPortal/NewsPaper/aboutme/views.py
+++ NewsPortal/NewsPaper/aboutme/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'aboutme/index.html')
+
+def contacts(request):
+    return render(request, 'aboutme/contacts.html')
+
+def aboutme(request):
+    return render(request, 'aboutme/aboutme.html')
