a25010bd34cbf37c78e0f0874ae6d6b7b717d612
nikvershinin <nik_vershinin@mail.ru>
2023-04-18 19:39:49 +0500

    continue

--- frontend/objects.js
+++ frontend/objects.js
@@ -44,3 +44,60 @@
 //     Задание 3.
 // Написать функцию, которая создает пустой объект, но без прототипа.
 //     При возникновении проблем по ходу решения вы всегда можете обратиться к ментору в Slack.
+// function obj(){
+//  return Object.create(null);
+// }
+// console.log (obj());
+
+//task4
+//Реализовать следующее консольное приложение подобно примеру,
+// который разбирался в видео. Реализуйте его на прототипах.
+// Определить иерархию электроприборов. Включить некоторые в розетку.
+// Посчитать потребляемую мощность.
+// Таких приборов должно быть, как минимум, два
+// (например, настольная лампа и компьютер).
+// Выбрав прибор, подумайте, какими свойствами он обладает.
+function Equipment(name, power){
+    this.name = name;
+    this.typeIs = 'электроприбор';
+    this.isOn = false
+    this.power = power;
+}
+Equipment.prototype.setisOn = function () {
+    this.isOn = true;
+}
+Equipment.prototype.setisOf = function () {
+    this.isOn = false;
+}
+Equipment.prototype.getisOn = function () {
+    if (this.isOn = true){
+        console.log(this.name+' включено')
+    }else {
+    console.log(this.isOn+ 'выключено')
+}}
+Equipment.prototype.powerAll = function () {
+    if (this.isOn = true){
+        console.log(this.name+' включено')
+    }else {
+        console.log(this.isOn+ 'выключено')
+    }}
+
+
+
+const light = new Equipment('Лампа', 30);
+const computer = new Equipment('Компьютер', 40);
+
+computer.setisOn();
+light.setisOn();
+
+function powerSum(args){
+    let result = 0;
+    for (let i in args){
+        console.log(i[this.power]);
+        result += i[this.power];
+    }
+
+    console.log(result);
+    return result;
+    }
+powerSum(computer)
